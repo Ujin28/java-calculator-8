2fde00ea2752892fb16f0e8a1db376517f562990
Ujin28 <melon4092@naver.com>
2025-10-20 23:23:29 +0900

    feat(application): 계산 결과 출력 기능 추가
    계산된 결과를 '결과 : {숫자}' 형식으로 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,5 +8,9 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해주세요");
         String snum = Console.readLine(); // 입력 예: "1,2,3" 또는 "//;\n1;2;3"
 
+        StringCalculator sc = new StringCalculator();
+        int result = sc.add(snum);
+        System.out.println("결과 : " + result);
+
     }
 }
